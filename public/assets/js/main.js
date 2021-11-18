36581d41c70aa89ff377e1c4f49ac4173c2d88bd
Natalia Ramirez Parra <jnataliaramirez@gmail.com>
2021-11-18 20:32:02 +0100

    Finish file listenChips

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -141,21 +141,20 @@ function renderChips() {
 
 function handleChip(ev) {
   const chipSelectId = ev.currentTarget.id;
- 
+
   // if there is a select repeat
-  const chipFound = chipsFilter.findIndex(chip => {
+  const chipFound = chipsFilter.findIndex((chip) => {
     return chip === chipSelectId;
-  })
+  });
   // add select for filter
   if (chipFound === -1) {
-    chipsFilter.push(chipSelectId)
+    chipsFilter.push(chipSelectId);
   } else {
-    chipsFilter.splice(chipFound, 1)
+    chipsFilter.splice(chipFound, 1);
   }
 
-
-  // chipsCompanies.classList.toggle('card__chips--select');
-
+  ev.currentTarget.classList.toggle('header__chips--select');
+  ev.currentTarget.classList.toggle('header__chips--unit');
 
   console.log(chipsFilter);
 }
